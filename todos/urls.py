35a0c82212d5a006ac32fef3399d1690ab7ca67a
yangyue <daisysong19900215@gmail.com>
2022-09-08 12:07:05 -0700

    Feature 8 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todos.views import TodoListListView
+from todos.views import TodoListListView, TodoListDetailView
 
 urlpatterns = [
     path('', TodoListListView.as_view(), name="todo_list_list"),
+    path("<int:pk>/", TodoListDetailView.as_view(), name="todo_list_detail")
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, UpdateView, DeleteView
-
-
-from .models import TodoList
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import TodoList, TodoItem
 
 
 class TodoListListView(ListView):
     model = TodoList
     template_name = "todo_list/list.html"
+
+
+class TodoListDetailView(DetailView):
+    model = TodoList
+    template_name = "todo_list/detail.html"
