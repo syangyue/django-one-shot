27cd72e3197b5efca606cbe5278297e133039772
yangyue <daisysong19900215@gmail.com>
2022-09-09 08:51:48 -0700

    done

--- brain_two/settings.py
+++ brain_two/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'todos',
+    "todos.apps.TodosConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- todos/models.py
+++ todos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TodoList(models.Model):
     name = models.CharField(max_length=100)
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class TodoList(models.Model):
 
 class TodoItem(models.Model):
     task = models.CharField(max_length=100)
-    due_date = models.DateTimeField(null=True)
+    due_date = models.DateTimeField(null=True, blank=True)
     is_completed = models.BooleanField(default=False)
     list = models.ForeignKey(
         TodoList, related_name="items", on_delete=models.CASCADE
